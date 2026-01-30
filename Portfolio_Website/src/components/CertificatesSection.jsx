28e5e965b15a9954d654136c17f3fa8830ed86c2
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2026-01-30 14:00:01 +0000

    Centering all certicate cards on the Certificates Section

--- Portfolio_Website/src/components/CertificatesSection.jsx
+++ Portfolio_Website/src/components/CertificatesSection.jsx
@@ -8,7 +8,7 @@ function CertificatesSection() {
         <h2 className="text-3xl md:text-5xl font-bold text-center mb-8">
           Certifications
         </h2>
-        <div className="max-w-3xl mx-auto px-6">
+        <div className="flex justify-center px-6">
           <Certificates />
         </div>
       </section>
