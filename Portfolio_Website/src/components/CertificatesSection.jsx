d7b24799f83882a86b3c6736ad6c48106e7b188b
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2026-01-31 12:49:30 +0000

    Styling CerficatesSection with tailwindcss

--- Portfolio_Website/src/components/CertificatesSection.jsx
+++ Portfolio_Website/src/components/CertificatesSection.jsx
@@ -5,8 +5,8 @@ function CertificatesSection() {
   return (
     <>
       <section id="certificates" className="py-1">
-        <h2 className="text-3xl md:text-5xl font-bold text-center mb-8">
-          Certifications
+        <h2 className="text-3xl md:text-5xl font-bold text-center mb-3">
+          Certificates
         </h2>
         <div className="flex justify-center px-6">
           <Certificates />
